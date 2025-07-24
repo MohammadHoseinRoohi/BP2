482bea41d1b47de21864f036b25996d1cce04bb7
mohammadHoseinRoohi <mohammadhoseinroohi4@gmail.com>
2025-07-25 02:37:08 +0330

    Issue#2

--- DTOs/Books/BookUpdateDto.cs
+++ DTOs/Books/BookUpdateDto.cs
@@ -7,11 +7,11 @@ namespace Practice2.DTOs.Books
 {
     public class BookUpdateDto
     {
-        public string? Title { get; set; }
-        public string? Writer { get; set; }
-        public string? Translator { get; set; }
-        public string? Publisher { get; set; }
-        public string? Genre { get; set; }
-        public double Price { get; set; }
+        public string? Title { get; set; } //***
+        public string? Writer { get; set; } //***
+        public string? Translator { get; set; } //***
+        public string? Publisher { get; set; } //***
+        public string? Genre { get; set; } //***
+        public double Price { get; set; } //***
     }
 }
